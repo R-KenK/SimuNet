59b7b740b1518bc1dd2771a18c0bf7c7aa5ece76
Kenneth Keuk <kenneth.keuk@gmail.com>
2021-01-14 19:03:38 +0900

    Saving WIP with commented remains of code
    checks ok

--- .WIP/WIP.R
+++ .WIP/WIP.R
@@ -21,3 +21,35 @@ minimize_NA<- function(scan){
   )
 }
 
+
+
+# manual tests to see if apply_mode does what it should, including on weighted networks ---------------
+
+
+test.scan.NA<- matrix(c(c(0,1,NA,0,NA),
+                        c(NA,0,1,1,NA),
+                        c(0,1,0,0,1),
+                        c(0,0,1,0,0),
+                        c(NA,1,0,0,0)),nrow = 5,byrow = TRUE,dimnames = list(letters[1:5],letters[1:5]))
+
+test<- matrix(c(0,1,0,1,
+                0,0,0,0,
+                1,0,0,1,
+                1,0,1,0),nrow = 4,byrow = TRUE,dimnames = list(letters[1:4],letters[1:4]))
+
+apply_mode(test,"directed")
+apply_mode(test,"undirected")
+apply_mode(test,"max")
+apply_mode(test,"min")
+apply_mode(test,"plus")
+
+test.weighted<- matrix(c(0,20,5,3,
+                         10,0,2,0,
+                         30,2,0,0,
+                         3,0,1,0),nrow = 4,byrow = TRUE,dimnames = list(letters[1:4],letters[1:4]))
+
+apply_mode(test.weighted,"directed")
+apply_mode(test.weighted,"undirected")
+apply_mode(test.weighted,"max")
+apply_mode(test.weighted,"min")
+apply_mode(test.weighted,"plus")
--- R/simu_scan.R
+++ R/simu_scan.R
@@ -7,12 +7,12 @@
 # #' @param sampling.param Optional if a theoretical scan is needed. Otherwise `samplingParam` object that stores:
 # #' \itemize{
 # #'   \item{method}{a character scalar, either:
-# #'     \item{"group"}{to use the group-scan sampling method}
-# #'     \item{"focal"}{to use the group-scan sampling method}
-# #'     \item{"both"}{to use both}
+# #'     \item{"group": to use the group-scan sampling method}
+# #'     \item{"focal": to use the group-scan sampling method}
+# #'     \item{"both": to use both}
 # #'   }
-# #'   \item{obs.prob}{an `obsProb` object.}
-# #'   \item{focal}{a `focal` object.}
+# #'   \item{obs.prob: an `obsProb` object.}
+# #'   \item{focal: a `focal` object.}
 # #' }
 # #' @param method Character scalar, specifies if the function should return a theoretical perfect group scan, an  empirical group scan (a similarly dimensioned matrix as Adj), or a focal scan (a vector representing the given focal's row in the group scan matrix). # TO REWRITE WITH ITEMS
 # #' @param obs.prob_fun Optional if a theoretical scan is needed.  either:
@@ -33,31 +33,34 @@
 # #' @export
 # #'
 # #' @examples
-# #' #' set.seed(42)
+# #' set.seed(42)
 # #'
-# #' n<- 5;nodes<- as.character(1:n);total_scan<- 42;
+# #' n<- 5;nodes<- letters[1:n];total_scan<- 42;
 # #' Adj<- matrix(data = 0,nrow = n,ncol = n,dimnames = list(nodes,nodes))
-# #' Adj[non.diagonal(Adj)]<- sample(0:42,n*(n-1),replace = TRUE)
+# #' Adj[non.diagonal(Adj)]<- sample(0:total_scan,n*(n-1),replace = TRUE)
 # #' Adj
 # #'
-# #' # Users can
-# #' generate_obs.prob(Adj,"directed",obs.prob_fun = "random")
-# #' generate_obs.prob(Adj,"directed",obs.prob_fun = 0.3)
+# #' # by default will simulate a directed theoretical scan
+# #' simu_scan(Adj,total_scan)
+# #'
+# #' # Users can generate sampling parameters to use in simu_scan
+# #' para.group.constant<- generate_samplingParam(method = "group",mode = "max",obs.prob = 0.42)
+# #' simu_scan(Adj,total_scan,para.group.constant)
 # #'
 # #' # using a user-defined function:
 # #' user_function.ij<- function(i,j,Adj) {i+j} # comparable to a dyad-trait-based bias
 # #' user_function.Adj<- function(i,j,Adj) {Adj*Adj} # comparable to a network-based bias
 # #'
-# #' generate_obs.prob(Adj,"directed",obs.prob_fun = user_function.ij)
-# #' generate_obs.prob(Adj,"directed",obs.prob_fun = user_function.Adj)
-# simu_scan<-function(Adj=NULL,total_scan=NULL,sampling.param = NULL,method = NULL,obs.prob_fun = NULL,focal.prob_fun = NULL,
+# #' generate_obsProb(Adj,"directed",obs.prob_fun = user_function.ij)
+# #' generate_obsProb(Adj,"directed",obs.prob_fun = user_function.Adj)
+# simu_scan<-function(Adj = NULL,total_scan = NULL,sampling.param = NULL,method = NULL,obs.prob_fun = NULL,focal.prob_fun = NULL,
 #                     mode = c("directed", "undirected", "max","min", "upper", "lower", "plus","vector")){
- #   mode<- match.arg(mode)
+#   mode<- match.arg(mode)
 #
 #   # Check if `Adj` has been passed as a `presenceProb` object or not, retrieve other variable otherwise if needed
-#   if (!is.presence.prob(Adj)) {
-#     Adj.subfun<- choose_Adj.subfun(mode)
-#     presence.prob<- generate_presence.prob(Adj = Adj,total_scan = total_scan,mode = mode,Adj.subfun = Adj.subfun)
+#   if (!is.presenceProb(Adj)) {
+#     Adj.subfun<- determine_Adj.subfun(mode)
+#     presence.prob<- generate_presenceProb(Adj = Adj,total_scan = total_scan,mode = mode,Adj.subfun = Adj.subfun)
 #   } else {
 #     presence.prob<- Adj
 #   }
@@ -69,9 +72,9 @@
 #   if (is.null(sampling.param) & is.null(method) & is.null(obs.prob_fun) & is.null(focal.prob_fun)) {
 #     scan
 #   } else {
-#     if(is.null(sampling.param)){
-#       sampling.param<- generate_sampling.param(method = method,mode = mode,obs.prob = obs.prob,focal.list = focal.list) # should return an error in case of missing parameters given the chosen method
+#     if (is.null(sampling.param)){
+#       sampling.param<- generate_samplingParam(method = method,mode = mode,obs.prob = obs.prob,focal.list = focal.list) # should return an error in case of missing parameters given the chosen method
 #     }
-#     sample_from_scan(scan = scan,sampling.param = sampling.param)
+#     generate_empiScan(scan = scan,sampling.param = sampling.param)
 #   }
 # }
