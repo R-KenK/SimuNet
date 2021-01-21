1077b8edb3e3c7d8f4a68f585ef55401324b5d3f
Kenneth Keuk <kenneth.keuk@gmail.com>
2021-01-21 20:30:34 +0900

    added sampling effort function `sum_scan.sampled`
    and wrapper to explicit `scans.to.do = "all"` into its vector form (used in print methods)

--- R/scan.sum_tools.R
+++ R/scan.sum_tools.R
@@ -10,14 +10,37 @@ sum_scan.list<- function(scan.list){
   Reduce(matrix_sum_na.rm,scan.list) # sum the scan list, considering NAs as zeros
 }
 
-# sum_sampled<- function(scan,method){
-#   switch(method,
-#          "theoretical" = {
-#            theoretical.sampled <- scan$theoretical.sum
-#            if (length(scan$scans.to.do) == 1) {if (scan$scans.to.do == "all") {scan$scans.to.do <- 1:scan$total_scan}}
-#            theoretical.sampled[scan$Adj.subfun(theoretical.sampled)] <- length(scan$scans.to.do)
-#            theoretical.sampled
-#          },
-#          ""
-#   )
-# }
+#' TO WRITE
+#'
+#' @param scan TO WRITE
+#' @param method TO WRITE
+#'
+#' @return TO WRITE
+#' @noRd
+sum_scan.sampled<- function(scan,method = c("theoretical","group","focal")) {
+  method <- match.arg(method)
+  switch(method,
+         "theoretical" = {
+           theoretical.sampled <- scan$Adj
+           non.diagonal(theoretical.sampled) <- length(explicit_scan.to.do(scan))
+           theoretical.sampled
+         },
+         "group" = count_NA(scan$group.scan.list),
+         "focal" = count_NA(scan$focal.scan.list)
+  )
+}
+
+#' TO WRITE
+#'
+#' @param scan TO WRITE
+#'
+#' @return TO WRITE
+#' @noRd
+explicit_scan.to.do <- function(scan) {
+  if (length(scan$scans.to.do) == 1) {
+    if (scan$scans.to.do == "all") {
+      return(1:scan$total_scan)
+    }
+  }
+  scan$scans.to.do
+}
