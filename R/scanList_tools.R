c42ba2ef0c1774573d9a499e34a8325a52ef671d
Kenneth Keuk <kenneth.keuk@gmail.com>
2021-07-23 22:07:08 +0900

    rewrote scanList generators and tools

---
+++ R/scanList_tools.R
@@ -0,0 +1,194 @@
+#' Generator for `scanList` objects
+#' Internal use. The user should rather rely on `simunet` as a wrapper for the
+#' different steps needed to perform the scan from inputted data.
+#'
+#' @param edge.Prob a `edgeProb` object
+#' @param n.scans TO WRIte
+#'
+#' @return a `scanList` object (S3 class) containing:
+#' \itemize{
+#'   \item{`raw.scan.list`: a list of raw binary adjacency matrix shaped like the
+#'   `Adj` contained in `edge.Prob`, considered directed in the
+#'   algorithm}
+#'   \item{`theoretical.scan.list`: a list of binary adjacency matrix, where all ties were
+#'   observed but the `mode` has been applied}
+#'   \item{`scanList.type`: character scalar. `generate_scan` sets it to
+#'   "theoretical", `sample_from_scan` will set it to "empirical" and append the
+#'   empirical matrix}
+#'   \item{`Adj`: `Adj` data contained in `edge.Prob`}
+#'   \item{`total_scan`: `total_scan` data contained in `edge.Prob`}
+#'   \item{`n.scans`: inputted `n.scans`}
+#'   \item{`mode`: `mode` data contained in `edge.Prob`}
+#'   \item{`Adj.subfun`: `Adj.subfun` data contained in `edge.Prob`}
+#'   \item{`edge.Prob`: `edge.Prob$P` (only the probability matrix) data}
+#'   \item{`use.snPackMat`: logical, if scans should be `snPackMat` objects or
+#'   regular matrices}
+#' }
+#'
+#' @noRd
+generate_scanList <- function(edge.Prob,n.scans){
+  raw.scanList <- draw_raw_scanList(edge.Prob = edge.Prob,n.scans = n.scans)
+  scanList <- apply_mode(raw.scanList = raw.scanList,mode = edge.Prob$mode)
+  attr(scanList,"attrs") <-
+    list(
+      scanList.type = "theoretical",
+      raw.scanList = raw.scanList,
+      Adj = edge.Prob$Adj,
+      samp.effort = edge.Prob$samp.effort,
+      n.scans = n.scans,
+      mode = edge.Prob$mode,
+      Adj.subfun = edge.Prob$Adj.subfun,
+      edge.Prob = edge.Prob$P # in here it is not a edgeProb object anymore, to avoid storing redundant variables,
+    )
+  class(scanList) <- c("theoretical","scanList")
+  scanList
+}
+
+#' Title
+#'
+#' @param scan.list
+#' @param exp.design
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate_empiscanList <- function(scan.list,exp.design) {
+  empiscanList <- exp.design$FUN.seq(scan.list)
+  attr(empiscanList,"attrs") <-
+    append(
+      get_attrs(scan.list),
+      list(theoretical.scanList = without_attrs(scan.list))
+    )
+  attrs(empiscanList,"scanList.type") <- "empirical"
+  class(empiscanList)<- c("empirical","scanList")
+  empiscanList
+}
+
+#' Title
+#'
+#' @param edge.Prob
+#' @param n.scans
+#'
+#' @return
+#' @export
+#'
+#' @examples
+draw_raw_scanList <- function(edge.Prob,n.scans) {
+  sL <- vapply(
+    1:n.scans,
+    \(s) {
+      stats::rbinom(edge.Prob$P,1L,edge.Prob$P)
+    },edge.Prob$Adj
+  )
+  class(sL) <- "scanList"
+  sL
+}
+
+# scanList tools ------------------------------------------------------------------------------
+
+#' Title
+#'
+#' @param scanList
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_attrs <- function(scanList) {
+  attr(scanList,"attrs")
+}
+
+#' Title
+#'
+#' @param scanList
+#'
+#' @return
+#' @export
+#'
+#' @examples
+without_attrs <- function(scanList) {
+  attr(scanList,"attrs") <- NULL
+  scanList
+}
+
+#' Title
+#'
+#' @param scanList
+#' @param a
+#'
+#' @return
+#' @export
+#'
+#' @examples
+attrs <- function(scanList,a = NULL) {
+  if (is.null(a)) return(get_attrs(scanList))
+  get_attrs(scanList)[[a]]
+}
+
+#' Title
+#'
+#' @param x
+#' @param which
+#' @param value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+`attrs<-` <- function(x,which,value) {
+  new <- get_attrs(x)
+  new[[which]] <- value
+  attr(x,"attrs") <- new
+  x
+}
+
+#' TO WRITE
+#'
+#' @param sL TO WRITE
+#' @param .f TO WRITE
+#' @param ... TO WRITE
+#' @param USE.NAMES TO WRITE
+#'
+#' @return TO WRITE
+#' @keywords internal
+#'
+#' @examples
+#' # TO WRITE
+sLapply <- function(sL,.f,...,USE.NAMES = TRUE) {
+  vapply(
+    X = 1:(dim(sL)[3]),
+    FUN = function(x) .f(sL[,,x]),
+    FUN.VALUE = sL[,,1],
+    ... = ...,
+    USE.NAMES = USE.NAMES
+  )
+}
+
+#' Title
+#'
+#' @param x
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.scanList <- function(x,...) {
+  print.default(without_attrs(x))
+  cat("\n\nHidden attributes:",names(get_attrs(x)))
+}
+
+#' TO WRITE
+#'
+#' @param x
+#'
+#' @return TO WRITE
+#' @keywords internal
+#'
+#' @examples
+#' # TO WRITE
+t.scanList <- function(x) {
+  aperm(x,c(2,1,3))
+}
+
