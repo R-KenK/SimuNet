a1c969c846712f03be0ebce8467d8fe1f07c12e7
Kenneth Keuk <kenneth.keuk@gmail.com>
2021-08-10 13:15:13 +0900

    written doc for building blocks

--- R/expDesign_building.blocks.R
+++ R/expDesign_building.blocks.R
@@ -5,22 +5,65 @@
 
 ## sum ----
 
-#'  TO WRITE
+#' Sum list of binary scans into a weighted adjacency matrix
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @param scan.list TO WRITE
-#' @param ... TO WRITE
+#' @param scan.list a `scanList` or `sLlist` object. See objects returned by
+#'   [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed.
 #'
-#' @return TO WRITE
+#' `sum_scans()` can use argument:
+#' * `which`: character scalar, either:
+#'   * `"auto"`: determine automatically which `scanList` to sum
+#'   * `"theoretical"`: sum the `theoretical.scanList` (useful in the case of empirical
+#'   `scanList`s)
+#'   * `"raw"`: sum the `raw.scanList` (useful to see the impact of chosen `mode`)
+#'
+#' @return a `sum` object, or list of such, consisting mainly on a weighted adjacency matrix where
+#'   each edge weight is equal to the sum of all binary edges. Inherits from the previous `scanList`
+#'   class (theoretical or empirical, inheriting from `scanList`), and keeps track of the
+#'   `scan.list`'s list of attributes `attrs`.
+#'
+#'   Also adds these attributes to `attrs`:
+#'   * `summed.scanList`: the original `scanList` (3D array) that has been summed
+#'   * `sampled`: an integer matrix representing how many time each edge has been sampled (i.e. was
+#'   not `NA`). Determined via [`count_nonNA()`][count_nonNA()]
+#'
 #' @export
 #'
+#' @seealso [simunet()], [design_exp()], [perform_exp()].
+#'
 #' @examples
-#' # TO WRITE
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' diag(Adj) <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "directed",n.scans = 120L)
+#' sL
+#' sL |> sum_scans()
+#'
+#' ## group-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6))) |> sum_scans()
+#'
+#' ## comparing group-scan and focal sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6)),
+#'                   design_exp(customize_sampling("focal","even"))
+#'                   ) |> sum_scans()
 sum_scans <- function(scan.list,...) {
   UseMethod("sum_scans")
 }
 
 
-#'  TO WRITE
+#' sum_scans method for `scanList` objects
 #' @export
 #' @noRd
 sum_scans.scanList <- function(scan.list,which = c("auto","theoretical","raw"),...) {
@@ -40,14 +83,14 @@ sum_scans.scanList <- function(scan.list,which = c("auto","theoretical","raw"),.
   summed
 }
 
-#'  TO WRITE
+#' sum_scans method for `sLlist` objects
 #' @export
 #' @noRd
 sum_scans.sLlist <- function(scan.list,which = c("auto","theoretical","raw"),...) {
   sLlapply(scan.list,sum_scans,which = which)
 }
 
-#'  TO WRITE
+#' sum_scans method for `empirical` (`scanList`) objects
 #' @export
 #' @noRd
 sum_scans.empirical <- function(scan.list,which = c("auto","theoretical","raw"),...) {
@@ -56,76 +99,145 @@ sum_scans.empirical <- function(scan.list,which = c("auto","theoretical","raw"),
 
 ## scale ----
 
-#'  TO WRITE
+#' Scale list of binary scans into a weighted adjacency matrix
+#' Scaling here is dividing by the sum of 1s by the number of time an edge has been observed
+#' (whether it was 0 or 1).
 #'
-#' @param summed TO WRITE
-#' @param ... TO WRITE
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @return TO WRITE
+#' @param scan.list a `scanList` or `sLlist` object. See objects returned by
+#'   [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed. At the moment `scale_scans()` does not use
+#'
+#' At the moment `scale_scans()` does not use additional argument, arguments passed will be ignored.
+#'
+#' @return a `scaled` object, or list of such, consisting mainly on a weighted adjacency matrix
+#'   where each edge weight is equal to the sum of all binary edges divided by the number of times
+#'   they have been sampled (determined via [`count_nonNA()`][count_nonNA()]). Inherits from `sum`
+#'   and the previous `scanList` class (theoretical or empirical, inheriting from `scanList`), and
+#'   keeps track of the `scan.list`'s list of attributes `attrs`
+#'
 #' @export
 #'
+#' @seealso [simunet()], [design_exp()], [perform_exp()], [sum_scans()].
+#'
 #' @examples
-#' # TO WRITE
-scale_scans <- function(summed,...) {
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' diag(Adj) <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "directed",n.scans = 120L)
+#' sL
+#' # scale_scans() can scale sum objects...
+#' sL |> sum_scans() |> scale_scans()
+#'
+#'
+#' # ... or scanList object directly
+#' ## group-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6))) |> scale_scans()
+#'
+#' ## comparing group-scan and focal sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6)),
+#'                   design_exp(customize_sampling("focal","even"))
+#'                   ) |> scale_scans()
+scale_scans <- function(scan.list,...) {
   UseMethod("scale_scans")
 }
 
-#'  TO WRITE
+#' scale_scans method for `sum` objects
 #' @export
 #' @noRd
-scale_scans.sum <- function(summed,...) {
-  sf <- attrs(summed,"Adj.subfun")
-  sampled <- attrs(summed,"sampled")
-  scaled <- summed
-  scaled[sf(scaled)] <- summed[sf(summed)] / sampled[sf(sampled)]
+scale_scans.sum <- function(scan.list,...) {
+  sf <- attrs(scan.list,"Adj.subfun")
+  sampled <- attrs(scan.list,"sampled")
+  scaled <- scan.list
+  scaled[sf(scaled)] <- scan.list[sf(scan.list)] / sampled[sf(sampled)]
   ifelse(!is.infinite(scaled),sampled,NA)
-  scaled <- copy_attrs_to(summed,scaled)
-  class(scaled) <- c("scaled",class(summed))
+  scaled <- copy_attrs_to(scan.list,scaled)
+  class(scaled) <- c("scaled",class(scan.list))
   scaled
 }
 
-#'  TO WRITE
+#' scale_scans method for `scanList` objects
 #' @export
 #' @noRd
-scale_scans.scanList <- function(summed,...) {
-  summed |>
+scale_scans.scanList <- function(scan.list,...) {
+  scan.list |>
     sum_scans() |>
     scale_scans()
 }
 
-#'  TO WRITE
+#' scale_scans method for `empirical` (`scanList`) objects
 #' @export
 #' @noRd
-scale_scans.empirical <- function(summed,...) {
+scale_scans.empirical <- function(scan.list,...) {
   NextMethod()
 }
 
-#'  TO WRITE
+#' scale_scans method for `sLlist` objects
 #' @export
 #' @noRd
-scale_scans.sLlist <- function(summed,...) {
-  sLlapply(summed,scale_scans)
+scale_scans.sLlist <- function(scan.list,...) {
+  sLlapply(scan.list,scale_scans)
 }
 
 ## count NAs ----
-#'  TO WRITE
+#' Count number of times edges have not been observed
+#' For empirical `scanList`, an edge that is not observed during a scan is `NA`. This function
+#' counts how many time this was the case in the inputted `scan.list` for each edge.
 #'
-#' @param scan.list TO WRITE
-#' @param ... TO WRITE
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @return TO WRITE
+#' @param scan.list a `scanList` or `sLlist` object, where an unobserved edge (whether it is 0 or 1)
+#'   is `NA`. See objects returned by [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed. At the moment `scale_scans()` does not use
+#'
+#' At the moment `count_NA()` does not use additional argument, arguments passed will be ignored.
+#'
+#' @return an integer matrix, or list of such, representing how many time each edge has been
+#'   unobserved (i.e. was `NA`). Inherits from the previous `scanList` class (theoretical or
+#'   empirical, inheriting from `scanList`), and keeps track of the `scan.list`'s list of attributes
+#'   `attrs`.
 #' @export
 #'
+#' @seealso [simunet()], [design_exp()], [perform_exp()], [count_nonNA()].
+#'
 #' @examples
-#' # TO WRITE
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' diag(Adj) <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "directed",n.scans = 120L)
+#' sL
+#'
+#' ## group-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6))) |> count_NA()
 count_NA <- function(scan.list,...) {
   UseMethod("count_NA")
 }
 
-#'  TO WRITE
+#' count_NA method for `scanList` objects
 #' @export
 #' @noRd
-count_NA.scanList <- function(scan.list,empirical = TRUE,...) {
+count_NA.scanList <- function(scan.list,...) {
   sf <- attrs(scan.list,"Adj.subfun")
   scan.sampled <- scan.list |> is.na() |> ifelse(1L,0L) %>% copy_attrs_to(from = scan.list)
   scan.sampled <- scan.sampled |> rowSums(na.rm = TRUE,dims = 2L)
@@ -133,39 +245,66 @@ count_NA.scanList <- function(scan.list,empirical = TRUE,...) {
   scan.sampled
 }
 
-#'  TO WRITE
+#' count_NA method for `empirical` (`scanList`) objects
 #' @export
 #' @noRd
-count_NA.empirical <- function(scan.list,empirical = TRUE,...) {
+count_NA.empirical <- function(scan.list,...) {
   NextMethod()
 }
 
-#'  TO WRITE
+#' count_NA method for `sLlist` objects
 #' @export
 #' @noRd
-count_NA.sLlist <- function(scan.list,empirical = TRUE,...) {
-  sLlapply(scan.list,count_NA,empirical = empirical)
+count_NA.sLlist <- function(scan.list,...) {
+  sLlapply(scan.list,count_NA)
 }
 
 ## count non NAs ----
 
-#' Count observed edges (non-`NA`s) for each edge in list of scans
-#' Internal use. Used to determine the sampling effort across all scans performed
+#' Count number of times edges have not been observed
+#' For empirical `scanList`, an edge that is observed during a scan is *not* `NA` (it will be either
+#' 0 or 1). This function counts how many time this was the case in the inputted `scan.list` for
+#' each edge.
 #'
-#' @param scan.list a list of binary adjacency matrices, where an unobserved dyad (whether it is theoretically 0 or 1) is `NA`
-#' @param ... TO WRITE
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @return an integer matrix representing the sampling effort for each dyad
+#' @param scan.list a `scanList` or `sLlist` object, where an unobserved edge (whether it is 0 or 1)
+#'   is `NA`. See objects returned by [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed. At the moment `scale_scans()` does not use
 #'
+#' At the moment `count_nonNA()` does not use additional argument, arguments passed will be ignored.
+#'
+#' @return an integer matrix, or list of such, representing how many time each edge has been sampled
+#'   (i.e. was *not* `NA`).Inherits from the previous `scanList` class (theoretical or empirical,
+#'   inheriting from `scanList`), and keeps track of the `scan.list`'s list of attributes `attrs`.
 #' @export
 #'
+#' @seealso [simunet()], [design_exp()], [perform_exp()]], [count_NA()].
+#'
 #' @examples
-#' # TO WRITE
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' diag(Adj) <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "directed",n.scans = 120L)
+#' sL
+#'
+#' ## group-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("group",.6))) |> count_nonNA()
 count_nonNA <- function(scan.list,...) {
   UseMethod("count_nonNA")
 }
 
-#'  TO WRITE
+#' count_nonNA method for `scanList` objects
 #' @export
 #' @noRd
 count_nonNA.scanList <- function(scan.list,...) {
@@ -176,14 +315,14 @@ count_nonNA.scanList <- function(scan.list,...) {
   scan.sampled
 }
 
-#'  TO WRITE
+#' count_nonNA method for `empirical` (`scanList`) objects
 #' @export
 #' @noRd
 count_nonNA.empirical <- function(scan.list,...) {
   NextMethod()
 }
 
-#'  TO WRITE
+#' count_nonNA method for `sLlist` objects
 #' @export
 #' @noRd
 count_nonNA.sLlist <- function(scan.list,...) {
@@ -192,21 +331,56 @@ count_nonNA.sLlist <- function(scan.list,...) {
 
 ## add scans ----
 
-#'  TO WRITE
+#' Perform additional scans and add them to the `scanList`
+#' New scans rely on edge probability matrix previously drawn from beta distributions. This can be
+#' used to compare sampling regime when it is expected that one will sample edges less in a
+#' predictable fashion (e.g. group-scan vs focal sampling)
 #'
-#' @param scan.list TO WRITE
-#' @param ... TO WRITE
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @return TO WRITE
+#' @param scan.list a `scanList` or `sLlist` object. See objects returned by
+#'   [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed. At the moment `scale_scans()` does not use
+#'
+#' `add_scans()` can use argument:
+#' * `new.scans`: integer scalar, the number of additional new scans to be performed and added to
+#' the `scan.list`
+#'
+#' @return a `scanList`, or list of such, to which additional scans have been added to the 3D array. See also [`simunet()`][simunet()].
+#'
+#'   Also modifies these attributes to `attrs`:
+#'   * `n.scans`: integer indicating how many scans have been performed in total. `n.scans` itself
+#'   has the attribute `scans.performed` that keeps track of what "batches" of scans have been
+#'   performed, by growing a vector of "scan batch" sizes
+#'
 #' @export
 #'
+#' @seealso [simunet()], [design_exp()], [perform_exp()].
+#'
 #' @examples
-#' # TO WRITE
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' diag(Adj) <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "directed",n.scans = 120L)
+#' sL
+#'
+#' ## focal-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("focal","even"))) |> add_scans(50)
 add_scans <- function(scan.list,...) {
   UseMethod("add_scans")
 }
 
-#'  TO WRITE
+#' add_scans method for `scanList` objects
 #' @export
 #' @noRd
 add_scans.scanList <- function(scan.list,new.scans,...) {
@@ -228,14 +402,14 @@ add_scans.scanList <- function(scan.list,new.scans,...) {
   new.scan.list
 }
 
-#'  TO WRITE
+#' add_scans method for `empirical` (`scanList`) objects
 #' @export
 #' @noRd
 add_scans.empirical <- function(scan.list,new.scans,...) {
   NextMethod()
 }
 
-#'  TO WRITE
+#' add_scans method for `sLlist` objects
 #' @export
 #' @noRd
 add_scans.sLlist <- function(scan.list,new.scans,...) {
@@ -244,21 +418,50 @@ add_scans.sLlist <- function(scan.list,new.scans,...) {
 
 ## remove peripheral individual ----
 
-#'  TO WRITE
+#' Remove from all scans the (overall) most peripheral individual
+#' Individual centrality based on eigen vectors. Mostly given as an example of experimental
+#' manipulations that could be performed on `scanList` as `expDesign`, even as user-defined
+#' functions
 #'
-#' @param scan.list TO WRITE
-#' @param ... TO WRITE
+#' Written as a S3 method to be applied to `scanList` or `sLlist` (list of `scanList`) objects.
 #'
-#' @return TO WRITE
+#' @param scan.list a `scanList` or `sLlist` object. See objects returned by
+#'   [`simunet()`][simunet()]
+#' @param ... additional arguments to be passed. At the moment `scale_scans()` does not use
+#'
+#' At the moment `remove_mostPeripheral()` does not use additional argument, arguments passed will be ignored.
+#'
+#' @return a `scanList`, or list of such, in which the most peripheral node has been removed. See
+#'   also [`simunet()`][simunet()].
+#'
+#' @seealso [simunet()], [design_exp()], [perform_exp()].
+#'
 #' @export
 #'
 #' @examples
-#' # TO WRITE
+#' set.seed(42)
+#' n <- 5L
+#' samp.effort <- 241L
+#'
+#' # Adjacency matrix import
+#' ## random directed adjacency matrix
+#' Adj <- sample(1:samp.effort,n * n) |>
+#'   matrix(nrow = 5,dimnames = list(letters[1:n],letters[1:n]))
+#' Adj[lower.tri(Adj,diag = FALSE)] <- 0L
+#' Adj
+#'
+#' # social network simulations
+#' ## theoretical scans
+#' sL <- simunet(Adj = Adj,samp.effort = samp.effort,mode = "upper",n.scans = 120L)
+#' sL
+#'
+#' ## focal-scan sampling
+#' sL |> perform_exp(design_exp(customize_sampling("focal","even"))) |> remove_mostPeripheral()
 remove_mostPeripheral <- function(scan.list,...) {
   UseMethod("remove_mostPeripheral")
 }
 
-#'  TO WRITE
+#' remove_mostPeripheral method for `scanList` objects
 #' @export
 #' @noRd
 remove_mostPeripheral.scanList <- function(scan.list,...) {
@@ -274,7 +477,7 @@ remove_mostPeripheral.scanList <- function(scan.list,...) {
     copy_attrs_to(from = scan.list)
 }
 
-#'  TO WRITE
+#' remove_mostPeripheral method for `sLlist` objects
 #' @export
 #' @noRd
 remove_mostPeripheral.sLlist <- function(scan.list,...) {
